de023b6eefcad3843efdee8bd21fbef39af130b1
chavo1 <42143239+chavo1@users.noreply.github.com>
2018-11-16 11:10:19 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,8 @@
 variable "access_key" {}
 variable "secret_key" {}
-variable "key_name" {}
+variable "key_name" {
+  description = "id_rsa"
+  }
 variable "region" {}
 variable "ami" {}
 variable "instance_type" {}
